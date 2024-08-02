9cd40447bc15e903b6a718f2901acb79f9a831d1
Eli-Raphael <104251394+Eli-Raphael@users.noreply.github.com>
2024-08-03 02:07:22 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
-    uint256 public balance;
+    mapping(address => uint256) public balances;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address indexed account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor() {
         owner = payable(msg.sender);
-        balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getBalance(address account) public view returns(uint256){
+        return balances[account];
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balances[msg.sender];
 
         // perform transaction
-        balance += _amount;
+        balances[msg.sender] += _amount;
 
         // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
+        assert(balances[msg.sender] == _previousBalance + _amount);
 
         // emit the event
-        emit Deposit(_amount);
+        emit Deposit(msg.sender, _amount);
     }
 
     // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
+        uint _previousBalance = balances[msg.sender];
+        if (balances[msg.sender] < _withdrawAmount) {
             revert InsufficientBalance({
-                balance: balance,
+                balance: balances[msg.sender],
                 withdrawAmount: _withdrawAmount
             });
         }
 
         // withdraw the given amount
-        balance -= _withdrawAmount;
+        balances[msg.sender] -= _withdrawAmount;
 
         // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+        assert(balances[msg.sender] == (_previousBalance - _withdrawAmount));
 
         // emit the event
-        emit Withdraw(_withdrawAmount);
+        emit Withdraw(msg.sender, _withdrawAmount);
     }
 }
